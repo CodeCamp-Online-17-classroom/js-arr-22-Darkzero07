2e9da04a8af79efd657b563953a596542dedc0d8
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-18 07:44:23 +0700

    js lab 108

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+function makeWorker() {
+  let name = 'Pete';
+  return function () {
+    alert(name);
+  };
+}
+let name = 'John';
+let work = makeWorker();
+work(); // *
+
+//ผลลัพธ์ในบรรทัดที่มี * มีค่าเป็นอะไร = pete
+
+//และเพราะอะไร เพราะ let name = "john" เป็นการ assign ค่านอก function makeworker() จึงไม่มีผลต่อค่าใน function
